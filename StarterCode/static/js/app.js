94a41994a3ea58d9a7eb76a8c8f91a030c07fedc
KidusGorfe <kidus.gorfe@gmail.com>
2024-01-03 13:58:40 -0600

    Final All Charts Added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -10,9 +10,10 @@ function init(data) {
         dropdownMenu.append("option").text(name).property("value", name);
     });
 
-    // Render the initial charts and metadata
+    // Render the initial charts and metadata for the first sample
     const firstSample = data.names[0];
     renderCharts(firstSample, data);
+    renderMetadata(firstSample, data);
 }
 
 function renderCharts(sample, data) {
@@ -33,11 +34,45 @@ function renderCharts(sample, data) {
     };
 
     Plotly.newPlot('bar', barData, barLayout);
+
+    // Bubble chart
+    var bubbleData = [{
+        x: selectedSample.otu_ids,
+        y: selectedSample.sample_values,
+        text: selectedSample.otu_labels,
+        mode: 'markers',
+        marker: {
+            size: selectedSample.sample_values,
+            color: selectedSample.otu_ids,
+            colorscale: "Earth"
+        }
+    }];
+
+    var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        xaxis: { title: "OTU ID" },
+        yaxis: { title: "Sample Values" },
+        margin: { t: 30 },
+        hovermode: "closest"
+    };
+
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+}
+
+function renderMetadata(sample, data) {
+    var metadata = data.metadata.filter(meta => meta.id == sample)[0];
+    var metadataPanel = d3.select("#sample-metadata");
+    metadataPanel.html(""); // Clear any existing metadata
+
+    Object.entries(metadata).forEach(([key, value]) => {
+        metadataPanel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
 }
 
 function optionChanged(newSample) {
     // Fetch new data each time a new sample is selected
     d3.json("samples.json").then(data => {
         renderCharts(newSample, data);
+        renderMetadata(newSample, data);
     });
 }
